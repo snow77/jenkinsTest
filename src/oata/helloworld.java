ba8993b9fc6db8e5f40ed18617cba5703ea61a3c
bingxing.liu <bingxing.liu@spreadtrum.com>
2015-12-18 14:36:16 +0800

    jhkj

--- src/oata/helloworld.java
+++ src/oata/helloworld.java
@@ -3,6 +3,6 @@ package oata;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("print d d")
+        System.out.println("jhgprint d d")
     }
 }
